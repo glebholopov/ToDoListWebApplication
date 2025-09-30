a38e73204f3ff4612082d5f02dc746730abc6c03
glebholopov <holopovg0@gmail.com>
2025-10-01 00:37:34 +0300

    Connection Database (PostgreSQL)

--- src/main/java/holopov/dev/dao/RecordDao.java
+++ src/main/java/holopov/dev/dao/RecordDao.java
@@ -1,41 +1,94 @@
 package holopov.dev.dao;
 
 import holopov.dev.entity.RecordStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import holopov.dev.entity.Record;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class RecordDao {
-    private final List<Record> records = new ArrayList<>(
-            Arrays.asList(
-                    new Record("Take a shower", RecordStatus.ACTIVE),
-                    new Record("Buy flowers", RecordStatus.DONE),
-                    new Record("Go to the gym", RecordStatus.ACTIVE)
-            )
-    );
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public RecordDao(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     public List<Record> findAllRecords() {
-        return new ArrayList<>(records);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+
+            Query query = entityManager.createQuery("SELECT r FROM Record r");
+            List<Record> records = query.getResultList();
+
+            entityManager.getTransaction().commit();
+            return records;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            entityManager.getTransaction().rollback();
+            return Collections.emptyList();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void saveRecord(Record record) {
-        records.add(record);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(record);
+            entityManager.getTransaction().commit();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void updateRecordStatus(int id, RecordStatus newStatus) {
-        for (Record record : records) {
-            if(record.getId() == id) {
-                record.setStatus(newStatus);
-                break;
-            }
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+
+            Query query = entityManager.createQuery("UPDATE Record SET status = :status WHERE id = :id");
+            query.setParameter("status", newStatus);
+            query.setParameter("id", id);
+            query.executeUpdate();
+
+            entityManager.getTransaction().commit();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
         }
     }
 
     public void deleteRecord(int id) {
-        records.removeIf(record -> record.getId() == id);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+
+            Query query = entityManager.createQuery("DELETE FROM Record WHERE id = :id");
+            query.setParameter("id", id);
+            query.executeUpdate();
+
+            entityManager.getTransaction().commit();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 }
--- src/main/java/holopov/dev/entity/Record.java
+++ src/main/java/holopov/dev/entity/Record.java
@@ -1,30 +1,48 @@
 package holopov.dev.entity;
 
-public class Record {
-    private static int counterSequence = 0;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-    private final int id;
-    private final String title;
+@Entity
+@Table(name = "records")
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "status",  nullable = false)
     private RecordStatus status;
 
+    public Record() { }
+
     public Record(String title) {
-        this.id = counterSequence++;
         this.title = title;
         this.status = RecordStatus.ACTIVE;
-    }
-
-    public Record(String title, RecordStatus status) {
-        this.id = counterSequence++;
-        this.title = title;
-        this.status = status;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public RecordStatus getStatus() {
